365c4c1c2c75a0683b067b3c978fe7c49d32627b
Oscar Swanros <oscar@swanros.com>
2019-07-22 09:56:01 -0500

    Create project.

---
+++ WorkshopDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkshopDiary
+//
+//  Created by Oscar Swanros on 7/22/19.
+//  Copyright © 2019 Pacific3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
